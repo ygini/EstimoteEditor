af028ac8027c8758950b41514d7764c6b6c6ac7c
Valérian "Nekith <nekith@gmail.com>
2014-01-21 12:16:09 +0100

    TestFlight integration

--- Estimote Editor/EEAppDelegate.m
+++ Estimote Editor/EEAppDelegate.m
@@ -6,13 +6,14 @@
 //  Copyright (c) 2013 Yoann Gini. All rights reserved.
 //
 
+#import "TestFlight.h"
 #import "EEAppDelegate.h"
-#import "EETableViewController.h"
 
 @implementation EEAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [TestFlight takeOff:@"c4af3c20-1a16-4ff2-9da8-0ad1e973b642"];
     return YES;
 }
 
