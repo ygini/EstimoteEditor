06257ae6eb13f792ea8a07292b59f4e0e8d52ab2
Valérian "Nekith <nekith@gmail.com>
2014-01-21 18:37:38 +0100

    Prepare original Estimote Editor pull request

--- Estimote Editor/EEAppDelegate.m
+++ Estimote Editor/EEAppDelegate.m
@@ -6,14 +6,12 @@
 //  Copyright (c) 2013 Yoann Gini. All rights reserved.
 //
 
-#import "TestFlight.h"
 #import "EEAppDelegate.h"
 
 @implementation EEAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [TestFlight takeOff:@"799d9126-b564-47fa-ad3e-384872a269ee"];
     return YES;
 }
 
